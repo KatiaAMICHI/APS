858d5a00221bcada6785a40a734d703232b43cf1
KatiaAmichi <amichi.katia@gmail.com>
2019-03-27 10:17:59 +0100

    begin APS1 (copy APS0 to APS1)

---
+++ src/main/APS1/syntaxe/parser.mli
@@ -0,0 +1,36 @@
+type token =
+  | ASTERISK
+  | COMMA
+  | ARROW
+  | COLON
+  | LPAR
+  | RPAR
+  | LCRO
+  | RCRO
+  | CONST
+  | IF
+  | FUN
+  | REC
+  | ECHO
+  | TRUE
+  | FALSE
+  | ADD
+  | MUL
+  | SUB
+  | DIV
+  | INTV of (int)
+  | BOOL
+  | INT
+  | IDENT of (string)
+  | PLUS
+  | MINUS
+  | SLASH
+  | EQ
+  | NOT
+  | AND
+  | OR
+  | LT
+  | PC
+
+val ansyn :
+  (Lexing.lexbuf  -> token) -> Lexing.lexbuf -> Ast.ansyn
